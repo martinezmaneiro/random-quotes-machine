bfc53f0d037db9836b89498dd8e370947a769f98
Joaquin Martinez <joaquinmar08@gmail.com>
2023-03-08 20:33:07 -0300

    [DEPLOY] changed src to import files

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import 'https://github.com/martinezmaneiro/random-quotes-machine/blob/main/src/index.css';
+import App from 'https://github.com/martinezmaneiro/random-quotes-machine/blob/main/src/App.js';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
